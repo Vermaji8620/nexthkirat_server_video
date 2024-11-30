cc18741b592a73439c19599df427d097508a20aa
AdityaRaj <xyz@gmai.com>
2024-11-30 22:03:46 +0530

    creating the backend done. data getting fetched successfully

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function GET() {
-
     return NextResponse.json({
         email: 'dadashj@dsfds.dfdf',
         name: "hkirat"
@@ -9,14 +8,11 @@ export function GET() {
 }
 
 export async function POST(req: NextRequest) {
-
-
     const body = await req.json();
     console.log(req.headers.get("authorization"))
     console.log(req.nextUrl.searchParams.get('name'))
-
     return NextResponse.json({
-        body
+        message: "ho gaya",
+        body,
     })
-
 }
---
+++ components/Signup.tsx
@@ -0,0 +1,45 @@
+"use client";
+import axios from "axios";
+import { useState } from "react";
+
+const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="flex flex-col gap-4 justify-center place-items-center h-screen">
+      <div>
+        username
+        <input
+          type="text"
+          placeholder="enter name"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          placeholder="enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="">
+        <button
+          type="button"
+          className="bg-red-600"
+          onClick={() => {
+            axios.post("http://localhost:3000/api/user", {
+                username, password
+            });
+          }}
+        >
+          signup
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
